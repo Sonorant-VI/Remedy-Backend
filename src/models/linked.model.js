8e60f22b1f0230b4e5e1fe1780a3a64fda8e691a
user <user@users-MacBook-Pro.local>
2021-03-28 14:23:28 +0100

    config and adding models

--- src/models/linked.model.js
+++ src/models/linked.model.js
@@ -2,11 +2,13 @@ module.exports = (sequelize, Sequelize) => {
     const Linked = sequelize.define("linked", {
       uid_linker: {
         type: Sequelize.INTEGER,
-        primaryKey: true
+        primaryKey: true,
+        allowNull: false
       }, 
       uid_linked: {
         type: Sequelize.INTEGER,
-        primaryKey: true
+        primaryKey: true,
+        allowNull: false
       }
     });
   
