752a506ba3d1b5fea2f55ef4174c6a1022252fc2
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2021-03-31 19:07:41 +0900

    appReminder

--- src/controllers/appReminder.controller.js
+++ src/controllers/appReminder.controller.js
@@ -1,17 +1,61 @@
 const db = require("../models");
-const appReminder = db.appreminder;
+const appReminder = db.AppReminder;
 const Op = db.Sequelize.Op;
 
+// Create and Save a new appReminder
 exports.create = (req, res) => {
 
-}
+    // console.log("Inside create reminder");
+    if (!req.body.start || !req.body.stop || !req.body.timeout || !req.body.reminderMsg ||!req.body.purpose || !req.body.patientId) {
+        res.status(400).send({
+            message: "Content can not be empty!"
+        });
+        return;
+    }
 
+    const appReminder = {
+        start: req.body.start,
+        stop: req.body.stop,
+        timeout: req.body.timeout,
+        purpose: req.body.purpose,
+        reminderMsg:req.body.reminderMsg,
+        patientId:req.body.patientId
+    };
+
+    console.log("Contents of body have been stored");
+    AppReminder.create(appReminder)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while creating the reminder."
+            });
+        });
+};
+//-------------------------------------------------------------------------------------------------------------------
+
+// Return all the appointment reminders for a single user
 exports.findAll = (req, res) => {
+    console.log("Inside find all appointment reminders");
+    const uid = req.params.uid;
+
+    AppReminder.findAll({ where: {patientId: uid}})
+        .then(data => {
+            res.send(data).status(200);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving reminders."
+            });
+        });
+       
     /*
     TODO adapt this
     const title = req.query.title;
     var condition = title ? { title: { [Op.iLike]: `%${title}%` } } : null;
-
     Tutorial.findAll({ where: condition })
         .then(data => {
             res.send(data);
@@ -22,11 +66,27 @@ exports.findAll = (req, res) => {
                     err.message || "Some error occurred while retrieving tutorials."
             });
         });
-
      */
 };
+   
+//-------------------------------------------------------------------------------------------------------------------
 
+// Return a single reminder for a specific user
 exports.findOne = (req, res) => {
+
+    console.log("Inside find one reminder");
+    const id = req.params.id;
+
+    AppReminder.findOne({where: {id: id, patientId: uid}})
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error retrieving Reminder with id=" + id
+            });
+        });
+
     /*
     TODO adapt this
     const id = req.params.id;
@@ -45,6 +105,46 @@ exports.findOne = (req, res) => {
 };
 
 exports.update = (req, res) => {
+
+    console.log("Inside update a single reminder");
+    const id = req.params.id;
+    
+    if (!req.body.start || !req.body.stop || !req.body.timeout || !req.body.reminderMsg ||!req.body.purpose || !req.body.patientId) {
+        res.status(400).send({
+            message: "Content can not be empty!"
+        });
+        return;
+    }
+
+    const appReminder = {
+        start: req.body.start,
+        stop: req.body.stop,
+        timeout: req.body.timeout,
+        purpose: req.body.purpose,
+        reminderMsg:req.body.reminderMsg,
+        patientId:req.body.patientId,
+        cancelled:req.body.cancelled
+    };
+
+    AppReminder.update(appReminder, {
+        where: { id: id }
+    })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Reminder was updated successfully."
+                });
+            } else {
+                res.send({
+                    message: `Cannot update Reminder with id=${id}. Maybe Tutorial was not found or req.body is empty!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error updating Reminder with id=" + id
+            });
+        });
     /*
     TODO adapt this
     const id = req.params.id;
@@ -73,6 +173,28 @@ exports.update = (req, res) => {
 };
 
 exports.delete = (req, res) => {
+    console.log("Inside destroy appointment reminder");
+    const id = req.params.id;
+
+    AppReminder.destroy({
+        where: { id: id }
+    })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Appointment Reminder was deleted successfully!"
+                });
+            } else {
+                res.send({
+                    message: `Cannot delete Reminder with id=${id}. Maybe reminder was not found!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Could not delete Reminder with id=" + id
+            });
+        });
     /*
     TODO adapt this
     const id = req.params.id;
