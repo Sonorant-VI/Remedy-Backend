bd0a19bfcf3f9cc8fb846214ae63fc2e106e9094
lansovinc <lan.sovinc@gmail.com>
2021-04-09 01:53:54 +0200

    Fixed auth tests, started working on appReminder tests.

--- tests/test.js
+++ tests/test.js
@@ -5,45 +5,35 @@ let server = require("../src/server.js");
 chai.use(chaiHttp);
 
 
+// Authentication ------------------------------------------------------------------------------------------------------
+let r = Math.random().toString(36).substring(7);
+let emailUser= r+'@gmail.com'
+let emailUser2= r+'@hotmail.com'
+let userID;
+let appReminderID;
 
-describe('/POST Register User', () => {
-    let r = Math.random().toString(36).substring(7);
-    let emailUser= r+'@gmail.com'
-    it('Create User', (done) => {
+describe('Authentication tests', () => {
+
+    it('Register User', (done) => {
         let user = {
             'email': emailUser,
             'password':'qwerty',
             'role':'active'
         }
-        console.log(r);
         chai.request(server)
             .post('/api/auth/register')
             .send(user)
             .end((err, res) => {
+                userID = res.body.uid
                 chai.expect(res.status).to.equal(400);
             });
         done();
     });
 
-    it('Create User wrong role ', (done) => {
+    it('Register User wrong role ', (done) => {
 
         let user = {
-            'email':'U89EZhoiflknsd@gmail.com',
-            'password':'qwerty',
-            'role':'1'
-        }
-        chai.request(server)
-            .post('/api/auth/register')
-            .send(user)
-            .end((err, res) => {
-                chai.expect(res.status).to.equal(400);
-            });
-        done();
-    });
-    it('Create User wrong email syntax ', (done) => {
-
-        let user = {
-            'email':'coms/skqosk/',
+            'email': emailUser2,
             'password':'qwerty',
             'role':'1'
         }
@@ -56,8 +46,93 @@ describe('/POST Register User', () => {
         done();
     });
 
+    it('Register User wrong email syntax ', (done) => {
 
-
+        let user = {
+            'email':'test',
+            'password':'qwerty',
+            'role':'1'
+        }
+        chai.request(server)
+            .post('/api/auth/register')
+            .send(user)
+            .end((err, res) => {
+                chai.expect(res.status).to.equal(400);
+            });
+        done();
+    });
 });
 
+// AppReminder ---------------------------------------------------------------------------------------------------------
+/*describe('AppReminder tests', () => {
 
+    it('Create AppReminder', (done) => {
+        let appReminder = {
+            "start": 123,
+            "stop": 456,
+            "timeout": 123,
+            "purpose": "test purpose",
+            "reminder_msg":"test message",
+            "patientId": userID
+        }
+        chai.request(server)
+            .post(`/api/appReminder/`)
+            .send(appReminder)
+            .end((err, res) => {
+                appReminderID = res.body.id
+                chai.expect(res.status).to.equal(200);
+            });
+        done();
+    });
+
+    it('Return all AppReminders', (done) => {
+        chai.request(server)
+            .get(`/api/appReminder/${userID}`)
+            .send()
+            .end((err, res) => {
+                chai.expect(res.status).to.equal(200);
+                chai.expect(res.body).should.be.a('array');
+            });
+        done();
+    });
+
+    it('Return AppReminder', (done) => {
+        chai.request(server)
+            .get(`/api/appReminder/${userID}/${appReminderID}`)
+            .send()
+            .end((err, res) => {
+                chai.expect(res.status).to.equal(200);
+            });
+        done();
+    });
+
+    it('Update AppReminder', (done) => {
+        let appReminder = {
+            "start": 123,
+            "stop": 456,
+            "timeout": 123,
+            "purpose": "test purpose",
+            "reminder_msg":"test message",
+            "patientId": userID,
+            "cancelled": 1
+        }
+        chai.request(server)
+            .patch(`/api/appReminder/${appReminderID}`)
+            .send()
+            .end((err, res) => {
+                chai.expect(res.status).to.equal(200);
+            });
+        done();
+    });
+
+    it('Delete AppReminder', (done) => {
+        chai.request(server)
+            .delete(`/api/appReminder/${appReminderID}`)
+            .send()
+            .end((err, res) => {
+                chai.expect(res.status).to.equal(200);
+            });
+        done();
+    });
+
+});*/
