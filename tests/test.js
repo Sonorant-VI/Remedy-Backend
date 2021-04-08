00ba3a652d2d32177da3ef3407e4ad5fd8860783
kabene <17karimabene@gmail.com>
2021-04-09 00:13:56 +0200

    tests clean template

--- tests/test.js
+++ tests/test.js
@@ -6,17 +6,56 @@ chai.use(chaiHttp);
 
 
 describe('/POST Register User', () => {
-    it('Create User Testing', (done) => {
+    it('Create User', (done) => {
+        let r = Math.random().toString(36).substring(7);
         let user = {
-            'email': 'test34@gmail.com',
+            'email': r+'@gmail.com',
             'password':'qwerty',
             'role':'active'
+        }
+        console.log(r);
+        chai.request(server)
+            .post('/api/auth/register')
+            .send(user)
+            .end((err, res) => {
+                chai.expect(res.status).to.equal(400);
+            });
+        done();
+    });
+
+    it('Create User wrong role ', (done) => {
+
+        let user = {
+            'email':'U89EZhoiflknsd@gmail.com',
+            'password':'qwerty',
+            'role':'1'
         }
         chai.request(server)
             .post('/api/auth/register')
             .send(user)
             .end((err, res) => {
-                chai.expect(res.status).to.equal(200);
+                chai.expect(res.status).to.equal(400);
             });
+        done();
     });
+    it('Create User wrong email syntax ', (done) => {
+
+        let user = {
+            'email':'coms/skqosk/',
+            'password':'qwerty',
+            'role':'1'
+        }
+        chai.request(server)
+            .post('/api/auth/register')
+            .send(user)
+            .end((err, res) => {
+                chai.expect(res.status).to.equal(400);
+            });
+        done();
+    });
+
+
+
 });
+
+
